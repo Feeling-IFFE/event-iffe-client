ef5dceee88418590af5d342c979e90a0cfe2e9b1
Alyssa DeMay <camocat11@gmail.com>
2020-08-28 09:06:23 -0500

    Token Auth working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,7 +20,7 @@ const unauthenticatedOptions = (
 
 const alwaysOptions = (
   <Fragment>
-    <Nav.Link exact href="#events">Home</Nav.Link>
+    <Nav.Link href="#events">Home</Nav.Link>
   </Fragment>
 )
 
