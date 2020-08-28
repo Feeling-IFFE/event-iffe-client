1161c58a9998daf16d90779271f946dc2ee9f13e
Alyssa DeMay <camocat11@gmail.com>
2020-08-28 15:33:40 -0500

    Fixed current events

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -63,10 +63,9 @@ class App extends Component {
           <AuthenticatedRoute user={user} path='/my-events' render={() => (
             <MyEvents user={user} />
           )} />
-          <Route user={user} path='/events' render={() => (
+          <Route user={user} exact path='/events' render={() => (
             <Events user={user} />
           )} />
-          <Route user={user} exact path='/events' component={Events} />
           <AuthenticatedRoute user={user} path='/create-event' component={CreateEvent} />
           <AuthenticatedRoute user={user} exact path='/events/:id' component={EventEdit} />
           <AuthenticatedRoute user={user} path='/event-edit' component={EventEdit} />
